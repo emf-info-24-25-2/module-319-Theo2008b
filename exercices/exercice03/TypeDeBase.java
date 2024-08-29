880ab21f59acf86242e1a57430fc424760770556
Bonvin-Théo-EMF <theo.bonvinm@studentfr.ch>
2024-08-29 11:37:55 +0200

    exercice03

---
+++ exercices/exercice03/TypeDeBase.java
@@ -0,0 +1,8 @@
+public class TypeDeBase {
+    public static void main(String[] args) {
+        int age = 16;
+        String nom = "Theo";
+        System.out.println("Je m'appele " +nom+ " et j'ai "+age+ "ans." );
+        System.out.println("dans 10 ans j'aurais " +(age+10)+" ans.");
+    }
+}
